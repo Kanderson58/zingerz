172a44203278a89eb741238aad96598ff1451e67
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-04-07 19:47:56 -0400

    refactor: Changed fetch to async await

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -3,21 +3,26 @@
   joke: string;
  }
 
-export const fetchJoke = () => {
-  return fetch('https://icanhazdadjoke.com/', {
+export const fetchJoke = async(): Promise<JokeResponse | null> => {
+   const response = await fetch('https://icanhazdadjoke.com/', {
     headers: {
       'Accept': 'application/json'
     }
-  })
-    .then(response => response.json())
-    .then((json: JokeResponse) => {
-      if (!json.joke) {
-        throw new Error('Failed to fetch joke');
-      }
-      return json;
-    })
-    .catch(error => {
-      console.error(error);
-      return null;
-    });
+  });
+    
+  const data = await response.json();
+  return data;
 };
+
+// .then(response => response.json())
+// .then((json: JokeResponse) => {
+//   if (!json.joke) {
+//     throw new Error('Failed to fetch joke');
+//   }
+//   return json;
+// })
+// .catch(error => {
+//   console.error(error);
+//   return null;
+// });
+// };
