da06ea365ea4811ea20a7c614c08a8537081fcaa
Kirk Hauck <kirkihauck@gmail.com>
2023-04-11 11:45:03 -0600

    feat: Setup search API call Co-authored-by: Kara Anderson <kanderson8585@gmail.com> Co-authored-by: Lauren Seward <lauren.unal@gmail.com> Co-authored-by: Gus Deribeaux <gusderibeaux@gmail.com>
    Signed-off-by: Kirk Hauck <kirkihauck@gmail.com>

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -3,24 +3,51 @@
   joke: string;
  }
 
+export interface SearchResponse {
+  "current_page": number,
+  "limit": number,
+  "next_page": number,
+  "previous_page": number,
+  "results": JokeResponse[],
+  "search_term": string,
+  "status": number,
+  "total_jokes": number,
+  "total_pages": number
+}
+
 export const fetchJoke = async(): Promise<JokeResponse | null> => {
   return await fetch('https://icanhazdadjoke.com/', {
     headers: {
       'Accept': 'application/json'
     }
-})
-  .then(response => response.json())
-  .then((json: JokeResponse) => {
-    if (!json.joke) {
-      throw new Error('Failed to fetch joke');
-    }
-    return json;
   })
-  .catch(error => {
-    console.error(error);
-    return null;
-  });
+    .then(response => response.json())
+    .then((json: JokeResponse) => {
+      if (!json.joke) {
+        throw new Error('Failed to fetch joke');
+      }
+      return json;
+    })
+    .catch(error => {
+      console.error(error);
+      return null;
+    });
 };
+
+export const fetchSearch = async(term: string): Promise<SearchResponse | null> => {
+  return await fetch(`https://icanhazdadjoke.com/search?term=${term}`, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to search jokes')
+      } else {
+        return response.json()
+      }
+    });
+}
 
 
 // export const fetchJoke = async(): Promise<JokeResponse | null> => {
