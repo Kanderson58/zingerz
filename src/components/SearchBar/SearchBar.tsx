e37f700c0f033c9ec7dabd4f58da9105081959c5
Kirk Hauck <kirkihauck@gmail.com>
2023-04-11 12:00:00 -0600

    Update SearchBar to successfully log search results in console Co-authored-by: Co-authored-by: Kara Anderson <kanderson8585@gmail.com> Co-authored-by: Lauren Seward <lauren.unal@gmail.com> Co-authored-by: Gus Deribeaux <gusderibeaux@gmail.com>
    Signed-off-by: Kirk Hauck <kirkihauck@gmail.com>

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,21 +1,31 @@
 import React, { useState, ChangeEvent } from 'react';
+import { fetchSearch } from '../../apiCalls';
 import './SearchBar.css';
 
 type Event = ChangeEvent<HTMLInputElement>
+type clickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>
 
 const SearchBar = () => {
-  const [search, setSearch] = useState('')
+  const [term, setTerm] = useState('')
+  //need to create error state
+
+  const searchJokes = (event: clickEvent) => {
+    event.preventDefault();
+    fetchSearch(term)
+      .then(data => console.log(data))
+      //need to make catch
+  }
 
   return (
     <form>
       <input
         type='text' 
-        name='search' 
+        name='term' 
         placeholder='Search jokes' 
-        value={search} 
-        onChange={(event: Event) => setSearch(event.target.value)} 
+        value={term} 
+        onChange={(event: Event) => setTerm(event.target.value)} 
       />
-      <button className='clear-btn'>X</button>
+      <button className='clear-btn' onClick={(event: clickEvent) => searchJokes(event)}>X</button>
     </form>
   );
 }
