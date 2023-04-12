b0a75e9b2cf82a54442b416e80e82c82907f0eba
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-04-12 18:54:53 -0400

    Create: search page testing

--- cypress/e2e/search-page-spec.cy.ts
+++ cypress/e2e/search-page-spec.cy.ts
@@ -1,14 +1,31 @@
 /// <reference types="Cypress" />
 
-const sampleJoke3 = 'Put the cat out … I did not realize it was on fire'
+const sampleJoke3 = {
+  "current_page": 1,
+  "limit": 20,
+  "next_page": 2,
+  "previous_page": 1,
+  "results": [
+    {
+      "id": "usrcaMuszd",
+      "joke": "What's the worst thing about ancient history class? The teachers tend to Babylon."
+    }
+  ],
+  "search_term": "",
+  "status": 200,
+  "total_jokes": 307,
+  "total_pages": 15
+}
 
 describe('Search View', () => {
   beforeEach('', () => {
+    console.log('sampleJoke3', sampleJoke3)
     cy.intercept('https://icanhazdadjoke.com/search?term=cat', sampleJoke3)
       .visit('http://localhost:3000/search');
   });
 
   it('should allow user to click and type in search bar', () => {
     cy.get('input').type('cat').get('.search-btn').click()
+    .get('.search-joke').should('be.visible')
   })
 })
