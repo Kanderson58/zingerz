76496290b6720263fbb5d7454b7bd6bdd1fa754a
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-04-12 19:13:26 -0400

    Create: sad path testing and go home testing

--- cypress/e2e/search-page-spec.cy.ts
+++ cypress/e2e/search-page-spec.cy.ts
@@ -25,7 +25,25 @@ describe('Search View', () => {
   });
 
   it('should allow user to click and type in search bar', () => {
-    cy.get('input').type('cat').get('.search-btn').click()
+    cy.get('input').type('teachers').get('.search-btn').click()
     .get('.search-joke').should('be.visible')
+    .contains('What\'s the worst thing about ancient history class? The teachers tend to Babylon.')
+  })
+  it('should be able to go back home', () => {
+    cy.get('.nav-links > [href="/"]').click()
+    cy.get('.main-joke')
+  })
+})
+
+describe('Sad Path', () => {
+  beforeEach('', () => {
+    cy.intercept('https://icanhazdadjoke.com/search?term=cat', { 
+      statusCode: 404
+    })
+    .visit('http://localhost:3000/search')
+  })
+  it.skip('should see 404 error', () => {
+    cy.get('input').type('cat').get('.search-btn').click()
+    .get('h2').contains('ERROR');
   })
 })
