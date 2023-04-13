e434b64f093eeeeb3e6a743d33e1994046d227b8
Kara Anderson <kanderson8585@gmail.com>
2023-04-13 16:03:25 -0600

    refac: make Joke component styling reliant on current URL path

--- src/components/Joke/Joke.tsx
+++ src/components/Joke/Joke.tsx
@@ -1,15 +1,18 @@
 import './Joke.css'
 import { JokeResponse } from '../../apiCalls'
+import { useLocation } from 'react-router-dom';
 
 interface Props {
   data: JokeResponse | null;
-  class: string;
   error?: string;
 }
 
-const Joke = ({data, error, class: Props}:Props) => {
+const Joke = ({data, error}:Props) => {
+  const location = useLocation();
+  const currentPath = (location.pathname === '/' ? 'main-joke' : 'search-joke');
+
   return (
-    <section className={Props}>
+    <section className={currentPath}>
       {data && !error ? data.joke : null}
       {error && <p className='home-error'>Sorry! {error} humor not found.</p>}
     </section>
