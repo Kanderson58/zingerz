aa801862d0fdf50babe4c9a56e7b093c7ff50f62
Lauren Seward <lauren.unal@gmail.com>
2023-04-13 16:49:46 -0500

    Bug: fix page refresh on search bar clear button

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { fetchSearch } from '../../apiCalls';
-import './SearchBar.css';
-import { SearchResponse } from '../../apiCalls';
-import Error from '../Error/Error';
+import React, { useState, useEffect } from "react";
+import { fetchSearch } from "../../apiCalls";
+import "./SearchBar.css";
+import { SearchResponse } from "../../apiCalls";
+import Error from "../Error/Error";
 
-type Event = React.ChangeEvent<HTMLInputElement>
-type ClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>
+type Event = React.ChangeEvent<HTMLInputElement>;
+type ClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>;
 type Props = {
-  displaySearch: (result: SearchResponse | null) => void
-}
+  displaySearch: (result: SearchResponse | null) => void;
+};
 
 const SearchBar = ({ displaySearch }: Props) => {
-  const [term, setTerm] = useState<string>('')
-  const [error, setError] = useState<string>('')
-  const [btnDisable, setBtnDisable] = useState<boolean>(true)
-  const [noResult, setNoResult] = useState<boolean>(false)
+  const [term, setTerm] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [btnDisable, setBtnDisable] = useState<boolean>(true);
+  const [noResult, setNoResult] = useState<boolean>(false);
 
   useEffect(() => {
-    setBtnDisable(!term)
-  }, [term])
+    setBtnDisable(!term);
+  }, [term]);
 
   const searchJokes = (event: ClickEvent) => {
-    setError('');
+    setError("");
     event.preventDefault();
     fetchSearch(term)
-      .then(data => {
-        if(!data?.results.length) {
+      .then((data) => {
+        if (!data?.results.length) {
           setNoResult(true);
           displaySearch(data);
         } else {
@@ -33,26 +33,33 @@ const SearchBar = ({ displaySearch }: Props) => {
           displaySearch(data);
         }
       })
-      .catch(error => setError(error.toString()))
-  }
+      .catch((error) => setError(error.toString()));
+  };
+
+  const clearSearch = (event: ClickEvent) => {
+    event.preventDefault();
+    setTerm("");
+    setNoResult(false);
+    setError("");
+  };
 
   return (
-    <div className='search-form'>
+    <div className="search-form">
       <form>
         <input
-        type='text' 
-        name='term' 
-        placeholder='Search jokes' 
-        value={term} 
-        onChange={(event: Event) => setTerm(event.target.value)} 
-      />
-      <button className='search-btn' disabled={btnDisable} onClick={searchJokes}>&#9906;</button>
-      <button className='clear-btn'>X</button>
+          type="text"
+          name="term"
+          placeholder="Search jokes"
+          value={term}
+          onChange={(event: Event) => setTerm(event.target.value)}
+        />
+        <button className="search-btn" disabled={btnDisable} onClick={searchJokes}>&#9906;</button>
+        <button className="clear-btn" onClick={clearSearch}>X</button>
       </form>
-      {noResult && <p className='no-result-msg'>Sorry! No funny business here, try searching again.</p>}
+      {noResult && (<p className="no-result-msg">Sorry! No funny business here, try searching again.</p>)}
       {error && <Error error={error} />}
     </div>
   );
-}
+};
 
 export default SearchBar;
