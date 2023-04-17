f5cb0f0bb284e7e933f89d19c9d030209798d60c
Kara Anderson <kanderson8585@gmail.com>
2023-04-17 15:15:14 -0600

    refac: Change basename

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import App from './components/App/App';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container);
-root.render(<BrowserRouter basename='zingerz'> <App /> </BrowserRouter>);
+root.render(<BrowserRouter basename=''> <App /> </BrowserRouter>);
